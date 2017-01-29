4c3dae1cb57aa3832e1818131c12a889aed73037
Derek Yeager <dcy2003@gmail.com>
2017-01-29 09:19:30 -0500

    Template validates and executes successfully

--- create-stack.sh
+++ create-stack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 aws cloudformation create-stack \
-    --stack-name UserDataTestStack \
-    --template-body file://cfn-template.yml \
-    --parameters ParameterKey=UserData,ParameterValue=`cat bootstrap.sh | base64` \
-    ParameterKey=AmiID,ParameterValue=ami-38cd975d \
-    ParameterKey=InstanceName,ParameterValue=TempTestInstance \
-    ParameterKey=SecurityGroupId,ParameterValue=sg-4ecb7c27
+    --stack-name MyTestStack \
+    --template-body file://cfn-template.json
+#    --parameters ParameterKey=UserData,ParameterValue=`cat bootstrap.sh | base64` \
+#    ParameterKey=AmiID,ParameterValue=ami-38cd975d \
+#    ParameterKey=InstanceName,ParameterValue=TempTestInstance \
+#    ParameterKey=SecurityGroupId,ParameterValue=sg-4ecb7c27
