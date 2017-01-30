384d96c1d42e4e94c8a28fa580e09a3c30d88581
Derek Yeager <dcy2003@gmail.com>
2017-01-29 20:33:43 -0500

    Added security group

--- create-stack.sh
+++ create-stack.sh
@@ -3,7 +3,8 @@
 aws cloudformation create-stack \
     --stack-name MyTestStack \
     --template-body file://cfn-template.json
-#    --parameters ParameterKey=UserData,ParameterValue=`cat bootstrap.sh | base64` \
+    --parameters ParameterKey=MyIP,ParameterValue=`curl -s http://whatismyip.akamai.com/`
+#    ParameterKey=UserData,ParameterValue=`cat bootstrap.sh | base64` \
 #    ParameterKey=AmiID,ParameterValue=ami-38cd975d \
 #    ParameterKey=InstanceName,ParameterValue=TempTestInstance \
 #    ParameterKey=SecurityGroupId,ParameterValue=sg-4ecb7c27
