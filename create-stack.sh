c0de257358c4e950594f62a66df119f77ab3842b
Derek Yeager <dcy2003@gmail.com>
2017-02-03 13:30:17 -0500

    UserData

--- create-stack.sh
+++ create-stack.sh
@@ -4,5 +4,5 @@ aws cloudformation create-stack \
     --stack-name MyTestStack \
     --template-body file://cfn-template.json \
     --parameters ParameterKey=MyIP,ParameterValue=`curl -s http://whatismyip.akamai.com/` \
-    ParameterKey=EC2SshKey,ParameterValue=cfn-example-key-pair
-#    ParameterKey=UserData,ParameterValue=`cat bootstrap.sh | base64`
+    ParameterKey=EC2SshKey,ParameterValue=cfn-example-key-pair \
+    ParameterKey=BootstrapScript,ParameterValue="`cat bootstrap.sh`"
