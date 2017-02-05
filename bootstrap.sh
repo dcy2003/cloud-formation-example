c14ebe71ff8529000e244ec2178201b4fd04580a
Derek Yeager <dcy2003@gmail.com>
2017-02-05 11:20:26 -0500

    git clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,11 +20,9 @@ chown ec2-user:ec2-user /home/ec2-user/.ssh/config
 
 # Add slave node to known_hosts
 slaveip=10.0.0.11
-ssh-keyscan $slaveip 2>/dev/null > slave.keys
-cat slave.keys >> /home/ec2-user/.ssh/known_hosts
+ssh-keyscan $slaveip >> /home/ec2-user/.ssh/known_hosts
 chmod 644 /home/ec2-user/.ssh/known_hosts
 chown ec2-user:ec2-user /home/ec2-user/.ssh/known_hosts
-rm slave.keys
 
 # Generate Ansible HOSTS file
 mkdir /etc/ansible
@@ -34,6 +32,17 @@ echo "10.0.0.11 ansible_user=ec2-user ansible_connection=ssh ansible_ssh_private
 chmod 644 /etc/ansible/hosts
 chown ec2-user:ec2-user /etc/ansible/hosts
 
+# Add GitHub and it IPs to root users known_hosts
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+for github_ip in `dig +short github.com`
+do
+  ssh-keyscan $github_ip >> ~/.ssh/known_hosts
+done
+
 # Download Ansible Playbook from Git
+cp /home/ec2-user/.ssh/config ~/.ssh/config
+chown root:root ~/.ssh/config
+git clone git@github.com:dcy2003/cloud-formation-example.git
+#chown -R ec2-user:ec2-user /home/ec2-user/cloud-formation-example/
 
 # Execute Playbook
