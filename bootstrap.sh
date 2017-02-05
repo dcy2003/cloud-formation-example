1ed55c811f228c2abaa4cb3d3405622aa00c25a1
Derek Yeager <dcy2003@gmail.com>
2017-02-05 17:59:04 -0500

    query infrastructure

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,17 +18,23 @@ aws s3 cp --region us-east-2 s3://asc-cloudformation-example/config /home/ec2-us
 chmod 644 /home/ec2-user/.ssh/config
 chown ec2-user:ec2-user /home/ec2-user/.ssh/config
 
-# Add slave node to known_hosts
-slaveip=10.0.0.11
-ssh-keyscan -H $slaveip >> /home/ec2-user/.ssh/known_hosts
+# Add slave nodes to known_hosts and ansible/hosts files
+mkdir /etc/ansible
+touch /etc/ansible/hosts
+
+echo "[slaves]" >> /etc/ansible/hosts
+
+slaveips=`aws ec2 describe-instances --filters "Name=tag:Name,Values=SlaveNode" | jq -r '.Reservations[].Instances[].NetworkInterfaces[] | .PrivateIpAddress'`
+
+for slaveip in $slaveips
+do
+  ssh-keyscan -H $slaveip >> /home/ec2-user/.ssh/known_hosts
+  echo "$slaveip ansible_user=ec2-user ansible_connection=ssh ansible_ssh_private_key_file=/home/ec2-user/.ssh/ssh-key.pem" >> /etc/ansible/hosts
+done
+
 chmod 644 /home/ec2-user/.ssh/known_hosts
 chown ec2-user:ec2-user /home/ec2-user/.ssh/known_hosts
 
-# Generate Ansible HOSTS file
-mkdir /etc/ansible
-touch /etc/ansible/hosts
-echo "[slaves]" >> /etc/ansible/hosts
-echo "10.0.0.11 ansible_user=ec2-user ansible_connection=ssh ansible_ssh_private_key_file=/home/ec2-user/.ssh/ssh-key.pem" >> /etc/ansible/hosts
 chmod 644 /etc/ansible/hosts
 chown ec2-user:ec2-user /etc/ansible/hosts
 
