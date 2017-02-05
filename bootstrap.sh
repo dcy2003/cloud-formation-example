96a51fbd92d8c301aea3d5a46af37011b8777995
Derek Yeager <dcy2003@gmail.com>
2017-02-04 23:42:17 -0500

    ssh stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,25 @@ sudo yum install git -y
 # install Ansible
 sudo pip install ansible
 
-# Download Git deployment key from S3
+# Download SSH key from S3
+aws s3 cp --region us-east-2 s3://asc-cloudformation-example/cfn-example-key-pair.pem /home/ec2-user/.ssh/ssh-key.pem
+chmod 400 /home/ec2-user/.ssh/ssh-key.pem
+chown ec2-user:ec2-user /home/ec2-user/.ssh/ssh-key.pem
 
 # Download ~/.ssh/config from S3
+aws s3 cp --region us-east-2 s3://asc-cloudformation-example/config /home/ec2-user/.ssh/config
+chmod 644 /home/ec2-user/.ssh/config
+chown ec2-user:ec2-user /home/ec2-user/.ssh/config
 
-# Download Ansible HOSTS file from S3
+slaveip=10.0.0.11
+ssh-keyscan $slaveip 2>/dev/null > slave.keys
+cat slave.keys >> /home/ec2-user/.ssh/known_hosts
+chmod 644 /home/ec2-user/.ssh/known_hosts
+chown ec2-user:ec2-user /home/ec2-user/.ssh/known_hosts
+rm slave.keys
 
-# Download Ansible Playbook from S3
+# Ansible HOSTS file
+
+# Download Ansible Playbook from Git
 
 # Execute Playbook
