5efd6a8c3375cedadde19b87ca946825a5d202be
Derek Yeager <dcy2003@gmail.com>
2017-02-06 10:52:36 -0500

    reuse

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-# NOTE: aws-cli and pip are preinstalled on Amazon Linux AMI
-
-# install Git
+# install git and jq
 sudo yum install git jq -y
 
-# install Ansible
+# install ansible
 sudo pip install ansible
 
-# Download SSH key from S3
-aws s3 cp --region us-east-2 s3://asc-cloudformation-example/cfn-example-key-pair.pem /home/ec2-user/.ssh/ssh-key.pem
-chmod 400 /home/ec2-user/.ssh/ssh-key.pem
-chown ec2-user:ec2-user /home/ec2-user/.ssh/ssh-key.pem
+# Variable Declarations
+region=us-east-2
+s3_bucket=s3://asc-cloudformation-example
+key_pair=cfn-example-key-pair.pem
+git_repo=git@github.com:dcy2003/cloud-formation-example.git
+temp_dir=/usr/tmp
+playbook_path=cloud-formation-example/ansible-playbook.yaml
+ssh_user=ec2-user
+slave_node_ssh_key=/home/$ssh_user/.ssh/ssh-key.pem
+ssh_config=/home/$ssh_user/.ssh/config
+ssh_known_hosts=/home/ec2-user/.ssh/known_hosts
 
+# NOTE: aws-cli and pip are preinstalled on Amazon Linux AMI
+
+# Download SSH key from S3
+aws s3 cp --region $region $s3_bucket/$key_pair $slave_node_ssh_key
+chmod 400 $slave_node_ssh_key
+chown $ssh_user:$ssh_user $slave_node_ssh_key
+
+# FIXME: dynamically generate slave entries
 # Download ~/.ssh/config from S3
-aws s3 cp --region us-east-2 s3://asc-cloudformation-example/config /home/ec2-user/.ssh/config
-chmod 644 /home/ec2-user/.ssh/config
-chown ec2-user:ec2-user /home/ec2-user/.ssh/config
+aws s3 cp --region $region $s3_bucket/config $ssh_config
+chmod 644 $ssh_config
+chown $ssh_user:$ssh_user $ssh_config
 
 # Add slave nodes to known_hosts and ansible/hosts files
 mkdir /etc/ansible
@@ -24,21 +37,19 @@ touch /etc/ansible/hosts
 
 echo "[slaves]" >> /etc/ansible/hosts
 
-# FIXME parameterize region
-slaveips=`aws ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=SlaveNode" | jq -r '.Reservations[].Instances[].NetworkInterfaces[] | .PrivateIpAddress'`
-
+slaveips=`aws ec2 describe-instances --region $region --filters "Name=tag:Name,Values=SlaveNode" | jq -r '.Reservations[].Instances[].NetworkInterfaces[] | .PrivateIpAddress'`
 for slaveip in $slaveips
 do
   echo "$slaveip"
-  ssh-keyscan -H $slaveip >> /home/ec2-user/.ssh/known_hosts
-  echo "$slaveip ansible_user=ec2-user ansible_connection=ssh ansible_ssh_private_key_file=/home/ec2-user/.ssh/ssh-key.pem" >> /etc/ansible/hosts
+  ssh-keyscan -H $slaveip >> $ssh_known_hosts
+  echo "$slaveip ansible_user=$ssh_user ansible_connection=ssh ansible_ssh_private_key_file=$slave_node_ssh_key" >> /etc/ansible/hosts
 done
 
-chmod 644 /home/ec2-user/.ssh/known_hosts
-chown ec2-user:ec2-user /home/ec2-user/.ssh/known_hosts
+chmod 644 $ssh_known_hosts
+chown $ssh_user:$ssh_user $ssh_known_hosts
 
 chmod 644 /etc/ansible/hosts
-chown ec2-user:ec2-user /etc/ansible/hosts
+chown $ssh_user:$ssh_user /etc/ansible/hosts
 
 # Add GitHub and it IPs to root users known_hosts
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
@@ -46,14 +57,14 @@ for github_ip in `dig +short github.com`
 do
   ssh-keyscan -H $github_ip >> ~/.ssh/known_hosts
 done
-cat /home/ec2-user/.ssh/known_hosts >> ~/.ssh/known_hosts
+cat $ssh_known_hosts >> ~/.ssh/known_hosts
 
 # Download Ansible Playbook from Git
-cp /home/ec2-user/.ssh/config ~/.ssh/config
+cp $ssh_config ~/.ssh/config
 chown root:root ~/.ssh/config
-cd /usr/tmp
-git clone git@github.com:dcy2003/cloud-formation-example.git
+cd $temp_dir
+git clone $git_repo
 
 # Execute Playbook
-# Specifying full path for ansible-playbook since it is not found in the path for the duration of userdata script for some reason
-/usr/local/bin/ansible-playbook /usr/tmp/cloud-formation-example/ansible-playbook.yaml
+# Specifying full path for ansible-playbook since it is not found in the path for the duration of userdata script
+/usr/local/bin/ansible-playbook $temp_dir/$playbook_path
