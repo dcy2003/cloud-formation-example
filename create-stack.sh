c9d0166f31a5717f55c9af6a966eb19e5946d335
Derek Yeager <dcy2003@gmail.com>
2017-02-07 08:49:48 -0500

    Progress on bootstrap script migration

--- create-stack.sh
+++ create-stack.sh
@@ -4,5 +4,6 @@ aws cloudformation create-stack \
     --stack-name MyTestStack \
     --template-body file://cfn-template.json \
     --parameters ParameterKey=MyIP,ParameterValue=`curl -s http://whatismyip.akamai.com/` \
-    ParameterKey=EC2SshKey,ParameterValue=cfn-example-key-pair \
+    ParameterKey=EC2SshKey,ParameterValue=cfn-key-pair \
+    ParameterKey=S3LocationOfPrivateKey,ParameterValue=s3://asc-cloudformation-resources/cfn-key-pair.pem \
     --capabilities CAPABILITY_NAMED_IAM
