2a6201861f704fb4d5d96d3da5c52d306a17fb88
Derek Yeager <dcy2003@gmail.com>
2017-02-07 13:42:07 -0500

    It works

--- create-stack.sh
+++ create-stack.sh
@@ -6,4 +6,6 @@ aws cloudformation create-stack \
     --parameters ParameterKey=MyIP,ParameterValue=`curl -s http://whatismyip.akamai.com/` \
     ParameterKey=EC2SshKey,ParameterValue=cfn-key-pair \
     ParameterKey=S3LocationOfPrivateKey,ParameterValue=s3://asc-cloudformation-resources/cfn-key-pair.pem \
+    ParameterKey=GitHubRepository,ParameterValue=git@github.com:dcy2003/cloud-formation-example.git \
+    ParameterKey=AnsiblePlaybook,ParameterValue=ansible-playbook.yaml \
     --capabilities CAPABILITY_NAMED_IAM
