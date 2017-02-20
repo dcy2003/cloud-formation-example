3b7d0204490e2a8769f59ca3b011de297ddf918f
Derek Yeager <dcy2003@gmail.com>
2017-02-19 19:10:32 -0500

    updates for contrived use case

--- create-stack.sh
+++ create-stack.sh
@@ -4,8 +4,5 @@ aws cloudformation create-stack \
     --stack-name MyTestStack \
     --template-body file://cfn-template.json \
     --parameters ParameterKey=MyIP,ParameterValue=`curl -s http://whatismyip.akamai.com/` \
-    ParameterKey=EC2SshKey,ParameterValue=cfn-key-pair \
-    ParameterKey=S3LocationOfPrivateKey,ParameterValue=s3://asc-cloudformation-resources/cfn-key-pair.pem \
-    ParameterKey=GitHubRepository,ParameterValue=git@github.com:dcy2003/cloud-formation-example.git \
-    ParameterKey=AnsiblePlaybook,ParameterValue=ansible-playbook.yaml \
+    ParameterKey=EC2SshKey,ParameterValue=canada-test-key-pair \
     --capabilities CAPABILITY_NAMED_IAM
